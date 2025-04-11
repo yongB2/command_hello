4ee1166883e4a0977908602c808a59ded1fabe4a
yongB2 <160087434+yongB2@users.noreply.github.com>
2025-04-11 09:54:13 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
+// For command line git tutorial
+// local reposiotory modification
+// github modification
+  
 print("hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
